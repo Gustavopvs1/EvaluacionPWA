d6c3dd7624ba6dcd8af3d32b55fd1c77c16a4f29
alexjamesmx <sg.alex32@gmail.com>
2024-03-25 16:27:14 -0600

    home, navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,30 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register("/sw.js");
 }
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register("/sw.js");
 }
 
-window.addEventListener('load', () => {
-    navigator.serviceWorker.addEventListener('message', event => {
-        const { type } = event.data;
-        if (type === 'online') {
-            alert('Conectado a internet.');
-        } else if (type === 'offline') {
-            alert('No hay conexión a internet, inténtalo de nuevo más tarde.');
-        }
-    });
+window.addEventListener("load", () => {
+  navigator.serviceWorker.addEventListener("message", (event) => {
+    const { type } = event.data;
+    if (type === "online") {
+      console.log("Conectado a internet.");
+    } else if (type === "offline") {
+      console.log("No hay conexión a internet, inténtalo de nuevo más tarde.");
+    }
+  });
 });
 
 function isOnline() {
-    if (navigator.onLine) {
-        alert('Conectado a internet.');
-    } else {
-        alert('No hay conexión a internet, inténtalo de nuevo más tarde.');
-    }
+  if (navigator.onLine) {
+    console.log("Conectado a internet.");
+  } else {
+    console.log("No hay conexión a internet, inténtalo de nuevo más tarde.");
+  }
 }
 
-window.addEventListener('online', isOnline);
-window.addEventListener('offline', isOnline);
+window.addEventListener("online", isOnline);
+window.addEventListener("offline", isOnline);
 
 isOnline();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useRoutes, BrowserRouter, } from 'react-router-dom'
-import Home from './pages/Home';
-import Perfil from './pages/Perfil';
+import React from "react";
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Perfil from "./pages/Perfil";
 
-import './App.css';
+import "./App.css";
+import Login from "./pages/Login";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    { path: '/perfil', element: <Perfil /> },
-    { path: '/', element: <Home /> },
-
-  ])
-  return routes
-}
+    { path: "/perfil", element: <Perfil /> },
+    { path: "/login", element: <Login /> },
+    { path: "/", element: <Home /> },
+  ]);
+  return routes;
+};
 const App = () => (
-  <div className='App'>
-    <BrowserRouter >
+  <div className="App">
+    <BrowserRouter>
       <AppRoutes />
-    </BrowserRouter >
+    </BrowserRouter>
   </div>
 );
 export default App;
-
